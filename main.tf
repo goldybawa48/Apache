13ed8f866ad99abd03a7cb54677484963776814f
Goldy <goldy@BAWA>
2024-08-09 05:18:09 +0530

    Testing

--- main.tf
+++ main.tf
@@ -3,13 +3,25 @@ provider "aws" {
 }
 
 resource "aws_instance" "goldy" {
-  ami             = "ami-04a81a99f5ec58529"
-  instance_type   = "t2.micro"
-  key_name        = "goldy"
-  security_groups = ["default"]
-  user_data       = file("${path.module}/userdata.sh")
+  ami           = "ami-04a81a99f5ec58529"  # Replace with a valid Ubuntu AMI ID for your region
+  instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraform"
-  }
+  # User data script to install Apache on Ubuntu
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
+              EOF
+
+  tags = { Name = "goldy" }
+}
+
+output "instance_id" {
+  value = aws_instance.instance1.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.instance1.public_ip
 }
--- userdata.sh
+++
@@ -1,4 +0,0 @@
-apt update -y
-apt install apache2
-systemctl start apache2
-systemctl enable apache2
